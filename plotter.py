c37bb323aed6c907564c027c9c87bde0b6bb686b
amandaabedahad <amanda.abedahad@outlook.com>
2022-01-05 11:27:10 +0100

    ändrade fontsize plot

--- plotter.py
+++ plotter.py
@@ -20,6 +20,9 @@ class Plotter:
 
         ax[0].clear()
         ax[1].clear()
+        plt.rcParams['font.family'] = "Times New Roman"
+        plt.rcParams.update({'font.size': 15})
+        csfont = {'fontname': 'Times New Roman'}
 
         for i, strat in enumerate(self.un):
             if strat in unique:
@@ -32,16 +35,18 @@ class Plotter:
                 ax[1].plot(x, self.unfracs[i], label='_nolegend_', linewidth=2.5)
             else:
                 ax[1].plot(x, self.unfracs[i], label=strat, linewidth=2.5)
-        ax[1].set_xlabel("Generation")
-        ax[1].set_ylabel("Fraction")
+        ax[1].set_xlabel("Generation", fontsize=14, font="Times New Roman")
+        ax[1].set_ylabel("Fraction", fontsize=14, font="Times New Roman")
         ax[1].legend(#bbox_to_anchor=(0, 1),
                          loc='best', borderaxespad=0)
+        ax[1].set_ylim([-0.01, 1.01])
+
 
         ax[0].plot(x, self.av, linewidth=2.5)
-        ax[0].set_xlabel("Generation")
-        ax[0].set_ylabel("Average Score")
+        ax[0].set_xlabel("Generation", fontsize=14, font="Times New Roman")
+        ax[0].set_ylabel("Average Score", fontsize=14, font="Times New Roman")
         plt.draw()
-        plt.pause(0.1)
+        plt.pause(0.001)
 
 
 
